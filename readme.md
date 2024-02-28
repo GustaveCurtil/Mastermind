7d2f600ac2cf0500e35a4ec275f80dc3312b7de4
Gustave Curtil <gustave.curtil@tutanota.com>
2024-02-28 10:16:25 +0100

    select a color and put it in the correct row + formatting

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Master Mind TD's
+
+1) Layout 
+2) Select a color
+3) Place the color on the board
+    - on the correct row
+    - have some margin
+4) Remove a pawn from the board
+
+-----
+
+5) 
--- script.js
+++ script.js
@@ -1,19 +1,90 @@
-let pawns = document.querySelectorAll('.pawn');
+let code = ['black', 'yellow', 'yellow', 'red'];
+
+let colors = document.querySelectorAll('.colorselection');
 let selectedColor;
 
-pawns.forEach(pawn => {
-    pawn.addEventListener('click', ()=> {
-        changeSelectedColor(pawn);
-        selectedColor = pawn.id;
-    })
-});
+let rows = document.querySelectorAll(".row");
+rows = [...rows].reverse();
+let placeholders;
 
+startGame()
+
+function startGame() {
+    colorSelection();
+    confirmFilledRow();
+    placeholders = checkActiveRow(0).querySelectorAll('.attempt .placeholder');
+    changeColorsOnActiveRow();
+
+    let button = document.querySelector('button')
+    button.addEventListener("click", ()=> {
+        if (checkForFullRow()) {
+            console.log(checkForFullRow())
+        }
+    })
+}
+
+function colorSelection() {
+    colors.forEach(pawn => {
+        pawn.addEventListener('click', ()=> {
+            changeSelectedColor(pawn);
+            selectedColor = pawn.id;
+        })
+    });
+}
 
 function changeSelectedColor(colorPawn) {
-    pawns.forEach(pawn => {
-        pawn.style.opacity = "var(--transparancy)";
+    colors.forEach(pawn => {
+        pawn.classList.remove("selected");
         if (pawn === colorPawn) {
-            pawn.style.opacity = 1;
+            pawn.classList.add("selected");
         }
     });
+}
+
+function confirmFilledRow() {
+    rows.forEach(row => {
+        row.played = false;
+        if (checkActiveRow(0) === row) {
+            let confirm = row.querySelector('.confirm');
+            confirm.innerHTML = "<button>ok</button>";
+        }
+    });
+}
+
+function checkActiveRow(i) {
+    if (rows[i].played === false) {
+        return rows[i];
+    } else {
+        checkActiveRow(i+1);
+    }
+}
+
+function changeColorsOnActiveRow() {
+    placeholders.forEach(placeholder => {
+        placeholder.addEventListener("click", ()=> {
+            if (selectedColor) {
+                placeholder.className = 'pawn ' + selectedColor;
+                placeholder.color = selectedColor;
+            }
+        })   
+         
+    });
+}
+
+
+
+
+function checkForFullRow() {
+    let code = [null, null, null, null]
+    for (let i = 0; i < placeholders.length; i++) {
+        if (placeholders[i].color) {
+            code[i] = placeholders[i].color;
+        }
+    }
+
+    if (!code.includes(null)) {
+        return code
+    } else {
+        return false
+    }
 }
